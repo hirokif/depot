a7d4276971d237bb7be3271a81a796b66d987a21
hirokif <hirokif@gmail.com>
2012-04-18 23:05:26 +0900

    p79 validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  varidates :title, uniqueness: true
-  vatidates :image_url, allow_blank: true, format: {
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
     with:    %r{\.(gif|ipg|png)$}i,
     message: 'はGIF、JPG、PNG画像のURLでなければなりません'
   }
